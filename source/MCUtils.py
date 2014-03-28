1ac0ab1e250cd1e708fc6f24e99b131658a05593
Chase Million <chase.million@gmail.com>
2014-03-28 16:49:28 -0400

    adding docstrings

--- source/MCUtils.py
+++ source/MCUtils.py
@@ -7,8 +7,8 @@ import time
 from astropy.io import fits as pyfits
 import csv
 
-# Print an error with callback and note
 def error(program,note=''):
+	"""Print ERROR with callback and note."""
         print '*** ERROR: '+str(program)
         if note:
                 print '    '+str(note)
@@ -17,10 +17,12 @@ def error(program,note=''):
 def area(radius):
 	return np.pi*radius**2.
 
-# Rotate vectors clockwise by theta degrees
-#  vector must have shape (2,n) and thata must have shape (n,)
-#  will return shape (2,n)
 def rotvec(vector,theta):
+	"""Rotate vectors clockwise by theta degrees
+	vector -- must have shape (2,n)
+	theta  -- must have shape (n,)
+	returns w/ shape (2,n)
+	"""
 	R = np.array([[np.cos(np.radians(theta)),-np.sin(np.radians(theta))],
 		      [np.sin(np.radians(theta)), np.cos(np.radians(theta))]])
 	return np.dot(R.T,vector)[0]
@@ -28,15 +30,15 @@ def rotvec(vector,theta):
 def rms(data):
         return np.sqrt(np.square(np.array(data)-np.array(data).mean()).mean())
 
-# Used for updating text output in place without doing a carriage return.
-def print_inline(text):
-        stdout.write("                                                                       \r")
+def print_inline(text,blanks=120):
+	"""For updating text in place without a carriage return."""
+        stdout.write(" "*blanks+"\r")
         stdout.write(str(text+'\r'))
         stdout.flush()
         return
 
-# A tool to make simple requests calls more robust against network issues.
 def manage_requests(query,maxcnt=20,wait=1):
+	"""Make simple 'requests' calls slightly more robust against network issues."""
 	cnt = 0
 	while cnt < maxcnt:
 		try:
@@ -55,8 +57,8 @@ def manage_requests(query,maxcnt=20,wait=1):
 	print '		'+str(query)
 	return 0
 
-# A tool to make simple grequest calls more robust against network issues.
 def manage_grequests(queries,maxcnt=20,wait=1):
+	"""Make simple 'grequests' calls slightly more robust against network issues."""
 	cnt = 0
 	while cnt < maxcnt:
 		try:
@@ -72,34 +74,41 @@ def manage_grequests(queries,maxcnt=20,wait=1):
 		print '		'+str(q)
 	return 0
 
-# Returns an array of indexes for which the input conditions are true.
-# - conditions must be a numpy array.
-# Example:
-# > a = np.array([1,2,3,4,5,6,7,8,9])
-# > wheretrue(a>5)
-# array([5, 6, 7, 8])
-# > a[wheretrue(a>5)]
-# array([6, 7, 8, 9])
 def wheretrue(conditions):
+	"""Returns indices for which the input conditions are true.
+	Conditions must be a numpy array.
+	Example:
+	> a = np.array([1,2,3,4,5,6,7,8,9])
+	> wheretrue(a>5)
+	array([5, 6, 7, 8])
+	> a[wheretrue(a>5)]
+	array([6, 7, 8, 9])
+	"""
 	return np.where(conditions==True)[0]
 
 # Returns an array of indexes for which the input conditions are false.
 # - as above, conditions must be a numpy array.
 # Example: as above
 def wherefalse(conditions):
+	"""Returns indices for which the input conditions are false.
+	See the docstring for wheretrue() for an example.
+	"""
 	return np.where(conditions==False)[0]
 
 def find_nearest_lower(array,value):
-        # This assumes that:
-        #   1. value is within the range of array
-        #   2. array is ordered
-        #   3. array has no gaps
+	"""Finds the index of the value in the array that is closest without going over.
+	This function assumes that:
+		1. value is within the range of array
+		2. array is ordered
+		3. array has no gaps
+	"""
         idx = (np.abs(array-value)).argmin()
         if array[idx]>value:
                 idx -= 1
         return idx
 
 def get_fits_data(filename,dim=0,verbose=1):
+	"""Reads FITS data. A wrapper for common pyfits commands."""
 	if verbose:
         	print "         ",filename
         hdulist = pyfits.open(filename,memmap=1)
@@ -108,12 +117,14 @@ def get_fits_data(filename,dim=0,verbose=1):
         return data
 
 def get_fits_header(filename):
+	"""Reads a FITS header. A wrapper for common pyfits commands."""
         hdulist = pyfits.open(filename,memmap=1)
         htab = hdulist[0].header
         hdulist.close()
         return htab
 
 def get_tbl_data(filename,comment='|'):
+	"""Reads data from a table into a numpy array."""
         f = open(filename)
         lines = f.readlines()
         tbl = []
@@ -125,9 +136,8 @@ def get_tbl_data(filename,comment='|'):
 
         return np.array(tbl,dtype='float64')
 
-# Produces an array of 2x1 delimiting ranges between a and b
-#  with the requested length
 def chunk(a,b,length=1,verbose=0):
+	"""Produces an array of 2x1 delimiting ranges between a and b with the requested length."""
 	if not length:
 		return [[a,b]]
 	else:
@@ -137,6 +147,7 @@ def chunk(a,b,length=1,verbose=0):
 		return arr.tolist()
 
 def chunks(array,length=10,verbose=0):
+	"""Takes an array of ranges and produces a new array of ranges within the initial array that are of the requested length (or smaller)."""
 	out = []
 	for a in array:
 		out += chunk(a[0],a[1],length=length,verbose=verbose)
