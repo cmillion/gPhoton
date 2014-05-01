a0a0a80d48dc657ceb0516623ed20306f7e8373c
Chase Million <chase.million@gmail.com>
2014-04-30 21:43:18 -0400

    error estimating function

--- source/gphoton_utils.py
+++ source/gphoton_utils.py
@@ -4,6 +4,7 @@ import csv
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from galextools import counts2mag, mag2counts
 
 def read_lc(ifile,header=False):
     """Reads a light curve CSV into a pandas DataFrame"""
@@ -27,4 +28,25 @@ def plot_lc(data_frame):
     """
     plt.plot(data_frame.index.values, data_frame["flux"], "ko")
     plt.show()
+
+def make_errors(mag,band,sigma=3,mode='mag'):
+	"""Give upper and lower expected bounds as a function of the nominal
+	magnitude of a source. Super userful for identifying outliers.
+	sigma = how many sigma out to set the bounds
+	mode = ['cps','mag'] - units in which to report bounds
+	"""
+	if mode!='cps' and mode!='mag':
+		print 'mode must be set to "cps" or "mag"'
+		exit(1)
+	x = np.arange(1,1600)
+	cnt = mag2counts(mag,band)
+	if mode=='mag':
+		y = counts2mag(cnt*x/x,band)
+		ymin = counts2mag((cnt*x/x)-sigma*np.sqrt(cnt*x)/x,band)
+		ymax = counts2mag((cnt*x/x)+sigma*np.sqrt(cnt*x)/x,band)
+	else:
+		ymin = (cnt*x/x)-sigma*np.sqrt(cnt*x)/x
+		ymax = (cnt*x/x)+sigma*np.sqrt(cnt*x)/x
+	return ymin, ymax
+
 
