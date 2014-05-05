490682a0e9b61bfbe7be20d46459267457e2f274
Chase Million <chase.million@gmail.com>
2014-05-05 08:58:02 -0400

    housekeeping

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 **gPhoton** is a project to calibrate, archive, and make available every photon event observed by the GALEX mission in an effort to dramatically improve the flexibility and utility of the data with specific emphasis on enabling short time domain photometric studies.
+
+If you use gPhoton software in your research, please cite:
+Million, C., Fleming, S. W., Shiao, B. (2014) gPhoton. Software available at https://github.com/cmillion/gPhoton
+
--- calpaper/src/photometry.py
+++
@@ -1,9 +0,0 @@
-# The canonical GALEX pipeline used the Source Extractor (SExtractor) toolkit
-#  to extract photometric information from images. gAperture uses naive aperture
-#  photometry. These methods needs to be compared. The most straightforward way
-#  to do this is to compare gAperture flux measurements to their corresponding
-#  MCAT or GCAT values for a large number of sources across the sky. Do this
-#  efficiently will require enhancement of gAperture with the capability to
-#  determine and automatically use "optimum" aperture parameters based upon the
-#  MCAT/GCAT entry for the target in question.
-
