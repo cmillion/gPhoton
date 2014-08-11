4b0a8b50c340889b4aca5d0dbcfe06d2ab06e6ce
Chase Million <chase.million@gmail.com>
2014-08-11 18:46:52 -0400

    minor discretize of functionality

--- source/galextools.py
+++ source/galextools.py
@@ -116,6 +116,27 @@ def deg2pix(skypos,skyrange,pixsz=0.000416666666666667):
 	#		 not catch the whole range, but all the values are meaningful.
 	return np.abs(np.floor(wcs.sip_pix2foc(wcs.wcs_world2pix([coo],1),1)[0]))[::-1]
 
+def flat_scale_parameters(band):
+    """Return the flat scaling parameters for a given band."""
+    if band=='NUV':
+        flat_correct = -0.0154
+        flat_t0 = 840418779.02
+        flat_correct_0 = 1.9946352
+        flat_correct_1 = -1.9679445e-09
+        flat_correct_2 = 9.3025231e-19
+    elif band=='FUV':
+        flat_correct = -0.0031
+        flat_t0 = 840418779.02
+        flat_correct_0 = 1.2420282
+        flat_correct_1 = -2.8843099e-10
+        flat_correct_2 = 0.000
+    else:
+        print "Band not specified."
+        exit(1)
+    # It turns out that flat_correct and flat_t0 never get used.
+    # They are only retained above for historical purposes.
+    return flat_correct_0, flat_correct_1, flat_correct_2
+
 def compute_flat_scale(t,band,verbose=0):
     """Return the flat scale factor for a given time.
     These are empirically determined linear scales to the flat field
@@ -125,32 +146,18 @@ def compute_flat_scale(t,band,verbose=0):
     """
     if verbose:
         print "Calculating flat scale for t=",t,", and band=",band
-    if band=='NUV':
-        flat_correct=-0.0154
-        flat_t0=840418779.02
-        flat_correct_0=1.9946352
-        flat_correct_1=-1.9679445e-09
-        flat_correct_2=9.3025231e-19
-    elif band=='FUV':
-        flat_correct=-0.0154
-        flat_t0=840418779.02
-        flat_correct_0=1.2420282
-        flat_correct_1=-2.8843099e-10
-        flat_correct_2=0.000
-    else:
-        print "Band not specified."
-
+    (flat_correct_0, flat_correct_1,
+                                flat_correct_2) = flat_scale_parameters(band)
     t = np.array(t)
     flat_scale=flat_correct_0+(flat_correct_1*t)+(flat_correct_2*t)*t
-
     # There's a bulk shift in the response after the CSP
     ix = np.where(t>=881881215.995)
     if len(ix[0]):
         try:
             flat_scale[ix] *= 1.018
         except:
-            # if it does not have '__getitem__'
-            flat_scale *= 1.018
+            # if it does not have '__getitem__' (because it's a scalar)
+            flat_scale *= 1.018 if t>=881881215.995 else 1.
 
     if verbose:
         print "         flat scale = ",flat_scale
