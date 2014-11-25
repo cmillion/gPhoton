f7d9852c80723019e460ba59b5e634d7a71866f0
Chase Million <chase.million@gmail.com>
2014-11-25 11:09:43 -0500

    gFind regression tests

--- source/gFind.py
+++ source/gFind.py
@@ -11,13 +11,15 @@ def gFind(band='both', detsize=1.25, exponly=False, gaper=False, maxgap=1.0,
     returns the total exposure time as a float.
 	"""
 	# Determine if we have to loop over both bands or just one.
-	if band.lower() == 'both':
-		all_bands = ['fuv','nuv']
+	if band.upper() == 'BOTH':
+		output = {'NUV':None,'FUV':None}
+	elif band.upper() in ['NUV','FUV']:
+		output = {band.upper():None}
 	else:
-		all_bands = [band]
+		raise SystemExit('Invalid band: {b}'.format(b=band))
 	all_expt = []
 
-	for this_band in all_bands:
+	for this_band in output.keys():
 		## Get valid time ranges.
 	       	ranges = dbt.fGetTimeRanges(this_band,skypos,maxgap=maxgap,
 									minexp=minexp,trange=trange,verbose=verbose,
@@ -26,15 +28,13 @@ def gFind(band='both', detsize=1.25, exponly=False, gaper=False, maxgap=1.0,
 		if not len(ranges):
 			if not quiet:
 				print 'No {band} exposure time in database.'.format(
-														band=this_band.upper())
-			return {'t0':[0],'t1':[0],'expt':[0]}
+																band=this_band)
 		else:
 			expt = (ranges[:,1]-ranges[:,0]).sum()
 			if not quiet:
 				print "{band}: {expt}s (raw) in {n} exposures.".format(
-								band=this_band.upper(),expt=expt,n=len(ranges))
+								band=this_band,expt=expt,n=len(ranges))
 			if not exponly:
-				if not quiet: print ''
 				if gaper:
 					f = '['
 					for r in ranges:
@@ -44,8 +44,9 @@ def gFind(band='both', detsize=1.25, exponly=False, gaper=False, maxgap=1.0,
 					for r in ranges:
 						if not quiet:
 							print '    [ %.3f' % r[0] + ', %.3f' % r[1] + ' ], %.3f' % (r[1]-r[0]) + ' seconds'
-			all_expt.append(expt)
-	return {'t0':ranges[:,0],'t1':ranges[:,1],'expt':all_expt}
+		output[this_band]={'expt':expt,'t0':ranges[:,0],'t1':ranges[:,1]}
+#			all_expt.append(expt)
+	return output #{this_band:{'t0':ranges[:,0],'t1':ranges[:,1],'expt':all_expt}}
 
 def setup_parser(iam='gfind'):
 	parser = argparse.ArgumentParser(description="Locate available data.")
