d7e854556f0b2647d0c1d444c12c11a9e8239ba2
parkus <parke.loyd@gmail.com>
2014-12-12 12:25:54 -0700

    removed try/except in getcurve

--- source/curvetools.py
+++ source/curvetools.py
@@ -263,24 +263,20 @@ def getcurve(band, ra0, dec0, radius, annulus=None, stepsz=None, lcurve={},
     # TODO: Add an ability to specify or exclude specific time ranges
     if verbose:
         mc.print_inline("Moving to photon level operations.")
-    try:
-        lcurve = quickmag(band, ra0, dec0, tranges, radius, annulus=annulus,
-                          stepsz=stepsz, verbose=verbose, coadd=coadd, 
-                          calpath=calpath)
-        lcurve['cps'] = lcurve['sources']/lcurve['exptime']
-        lcurve['cps_bgsub'] = (lcurve['sources']-lcurve['bg']['simple'])/lcurve['exptime']
-        lcurve['cps_bgsub_cheese'] = (lcurve['sources']-lcurve['bg']['cheese'])/lcurve['exptime']
-        lcurve['mag'] = gxt.counts2mag(lcurve['cps'],band)
-        lcurve['mag_bgsub'] = gxt.counts2mag(lcurve['cps_bgsub'],band)
-        lcurve['mag_bgsub_cheese'] = gxt.counts2mag(lcurve['cps_bgsub_cheese'],band)
-        lcurve['flux'] = gxt.counts2flux(lcurve['cps'],band)
-        lcurve['flux_bgsub'] = gxt.counts2flux(lcurve['cps_bgsub'],band)
-        lcurve['flux_bgsub_cheese'] = gxt.counts2flux(lcurve['cps_bgsub_cheese'],band)
+        
+    lcurve = quickmag(band, ra0, dec0, tranges, radius, annulus=annulus,
+                      stepsz=stepsz, verbose=verbose, coadd=coadd, 
+                      calpath=calpath)
+    lcurve['cps'] = lcurve['sources']/lcurve['exptime']
+    lcurve['cps_bgsub'] = (lcurve['sources']-lcurve['bg']['simple'])/lcurve['exptime']
+    lcurve['cps_bgsub_cheese'] = (lcurve['sources']-lcurve['bg']['cheese'])/lcurve['exptime']
+    lcurve['mag'] = gxt.counts2mag(lcurve['cps'],band)
+    lcurve['mag_bgsub'] = gxt.counts2mag(lcurve['cps_bgsub'],band)
+    lcurve['mag_bgsub_cheese'] = gxt.counts2mag(lcurve['cps_bgsub_cheese'],band)
+    lcurve['flux'] = gxt.counts2flux(lcurve['cps'],band)
+    lcurve['flux_bgsub'] = gxt.counts2flux(lcurve['cps_bgsub'],band)
+    lcurve['flux_bgsub_cheese'] = gxt.counts2flux(lcurve['cps_bgsub_cheese'],band)
 
-    except:
-        lcurve = {'cps':[],'cps_bgsub':[],'cps_bgsub_cheese':[],
-                 'mag':[],'mag_bgsub':[],'mag_bgsub_cheese':[],
-                 'flux':[],'flux_bgsub':[],'flux_bgsub_cheese':[]}
     if verbose:
         mc.print_inline("Done.")
         mc.print_inline("")
@@ -316,6 +312,7 @@ def write_curve(band, ra0, dec0, radius, csvfile=None, annulus=None,
             test.to_csv(csvfile,index=False)
         except:
             print 'Failed to write to: '+str(csvfile)
+            raise
     else:
         if verbose>2:
             print "No CSV file requested."
