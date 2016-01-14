feb27dc7db2698355b5d7823aa3d313e0e882910
Scott Fleming <fleming@stsci.edu>
2016-01-14 11:54:20 -0500

    - Fixed unit tests to reflect latest code changes.

--- gPhoton/tests/test_gAperture_regr.py
+++ gPhoton/tests/test_gAperture_regr.py
@@ -35,32 +35,32 @@ class TestRegression(unittest.TestCase):
         out = ga.gAperture(band='NUV',skypos=self.skypos,radius=self.radius)
         self.assertEqual(len(out['exptime']),6)
         # Regtest the exposure times
-        self.assertAlmostEqual(out['exptime'][0],102.1226448)
-        self.assertAlmostEqual(out['exptime'][1],635.79861454)
-        self.assertAlmostEqual(out['exptime'][2],358.3098205)
-        self.assertAlmostEqual(out['exptime'][3],1102.0230354)
-        self.assertAlmostEqual(out['exptime'][4],97.32780661)
-        self.assertAlmostEqual(out['exptime'][5],829.32579227)
+        self.assertAlmostEqual(out['exptime'][0],101.31698158)
+        self.assertAlmostEqual(out['exptime'][1],635.41801554)
+        self.assertAlmostEqual(out['exptime'][2],357.71295862)
+        self.assertAlmostEqual(out['exptime'][3],1102.06018543)
+        self.assertAlmostEqual(out['exptime'][4],96.51055404)
+        self.assertAlmostEqual(out['exptime'][5],829.06433301)
         # Regtest the source magnitudes (no bg subtraction)
-        self.assertAlmostEqual(out['mag'][0],17.80444981)
-        self.assertAlmostEqual(out['mag'][1],17.78115675)
-        self.assertAlmostEqual(out['mag'][2],17.83393323)
-        self.assertAlmostEqual(out['mag'][3],13.16827634)
-        self.assertAlmostEqual(out['mag'][4],17.84024741)
-        self.assertAlmostEqual(out['mag'][5],17.79907901)
+        self.assertAlmostEqual(out['mag'][0],17.79585028)
+        self.assertAlmostEqual(out['mag'][1],17.78050662)
+        self.assertAlmostEqual(out['mag'][2],17.83212313)
+        self.assertAlmostEqual(out['mag'][3],13.16831294)
+        self.assertAlmostEqual(out['mag'][4],17.83109209)
+        self.assertAlmostEqual(out['mag'][5],17.79873666)
 
     def test_basic_query_FUV(self):
         """Regtest the simplest valid query. (FUV)"""
         out = ga.gAperture(band='FUV',skypos=self.skypos,radius=self.radius)
         self.assertEqual(len(out['exptime']),3)
         # Regtest the exposure times
-        self.assertAlmostEqual(out['exptime'][0],1229.27537977)
-        self.assertAlmostEqual(out['exptime'][1],108.03763929)
-        self.assertAlmostEqual(out['exptime'][2],917.04785491)
+        self.assertAlmostEqual(out['exptime'][0],1228.35811301)
+        self.assertAlmostEqual(out['exptime'][1],107.05101862)
+        self.assertAlmostEqual(out['exptime'][2],913.1313701)
         # Regtest the magnitudes (no bg subtraction)
-        self.assertAlmostEqual(out['mag'][0],13.55116246)
-        self.assertAlmostEqual(out['mag'][1],19.17924678)
-        self.assertAlmostEqual(out['mag'][2],19.0715966)
+        self.assertAlmostEqual(out['mag'][0],13.550352)
+        self.assertAlmostEqual(out['mag'][1],19.16928608)
+        self.assertAlmostEqual(out['mag'][2],19.06694976)
 
     def test_lcurve_query_NUV(self):
         """Regtest lightcurve over a specific time range. (NUV)"""
@@ -72,18 +72,20 @@ class TestRegression(unittest.TestCase):
         for binsize in (out['t1']-out['t0'])[:-1]:
             self.assertAlmostEqual(binsize,self.stepsz)
         # Check the magnitudes (no bg subtraction)
-        for i,mag in enumerate([ 17.21694316, 17.11307157, 17.00687142,
-                                 16.88015838, 16.59671077, 15.66001062,
-                                 12.90278139, 12.61704438, 12.64491365,
-                                 11.96610893, 12.27605849, 12.57045386,
-                                 12.76668811 ]):
+        for i,mag in enumerate(
+                               [ 17.19996981, 17.10094944, 16.99299101,
+                                 16.86953449, 16.58609703, 15.64080472,
+                                 12.8843604,  12.60970683, 12.63479485,
+                                 11.95780728, 12.2635085,  12.553789,
+                                 12.76446021 ]):
             self.assertAlmostEqual(out['mag'][i],mag)
         # Check the annulus background-subtracted fluxes (as magnitudes).
-        for i,mag_bgsub in enumerate([ 17.60929357, 17.45840616, 17.30776929,
-                                       17.13685966, 16.79604557, 15.7404064,
-                                       12.90954336, 12.62263757, 12.6503745,
-                                       11.97017948, 12.28061957, 12.57599662,
-                                       12.7731486 ]):
+        for i,mag_bgsub in enumerate(
+                                     [ 17.58702551, 17.44381185, 17.29197581,
+                                       17.12539048, 16.78432337, 15.71999064,
+                                       12.89102971, 12.61527998, 12.64022502,
+                                       11.96186916, 12.268034,   12.55925879,
+                                       12.77103454 ]):
             self.assertAlmostEqual(out['mag_bgsub'][i],mag_bgsub)
         # Check the annulus background values (counts, flat-weighted, and
         # within aperture).
@@ -92,37 +94,39 @@ class TestRegression(unittest.TestCase):
                                        2341., 3248., 2735., 2530.,
                                        1057. ]):
             self.assertAlmostEqual(out['bg_counts'][i],bg_counts)
-        for i,bg_flat_counts in enumerate([ 1881.16239462, 1861.18207487,
-                                            1825.13672239, 1784.60944618,
-                                            1846.24637584, 1861.00296681,
-                                            2044.22407791, 2197.61381261,
-                                            2090.54813136, 2898.65336221,
-                                            2442.5370132,  2261.0528663,
-                                            944.66126792 ]):
+        for i,bg_flat_counts in enumerate(
+                                          [ 1871.97722712, 1853.40782629,
+                                            1821.46503959, 1780.35790353,
+                                            1837.85420049, 1849.55875876,
+                                            2032.06998278, 2184.33106164,
+                                            2079.20667638, 2888.03488141,
+                                            2429.30234821, 2245.24240439,
+                                            941.65448052 ]):
             self.assertAlmostEqual(out['bg_flat_counts'][i],bg_flat_counts)
-        for i,bg in enumerate([ 376.23247892, 372.23641497, 365.02734448,
-                                356.92188924, 369.24927517, 372.20059336,
-                                408.84481558, 439.52276252, 418.10962627,
-                                579.73067244, 488.50740264, 452.21057326,
-                                188.93225358 ]):
+        for i,bg in enumerate(
+                              [ 374.39544542, 370.68156526, 364.29300792,
+                                356.07158071, 367.5708401,  369.91175175,
+                                406.41399656, 436.86621233, 415.84133528,
+                                577.60697628, 485.86046964, 449.04848088,
+                                188.3308961 ]):
             self.assertAlmostEqual(out['bg'][i],bg)
         # Check the MCAT per-visit background subtracted fluxes.
-        for i,mag_mcatbgsub in enumerate([ 17.73718973, 17.57407121,
-                                           17.4156312, 17.23557599,
-                                           16.85957154, 15.76329305,
-                                           12.91058331, 12.62303601,
-                                           12.65106151, 11.96939464,
-                                           12.28043197, 12.57619315,
-                                           12.77356797]):
+        for i,mag_mcatbgsub in enumerate(
+                                         [ 17.70991261, 17.55559111,
+                                           17.39546401, 17.22086535,
+                                           16.84606843, 15.74218895,
+                                           12.8920306,  12.61565799,
+                                           12.64088552, 11.96106793,
+                                           12.26783162, 12.55944065,
+                                           12.77132593 ]):
             self.assertAlmostEqual(out['mag_mcatbgsub'][i],mag_mcatbgsub)
         # Check the MCAT per-visit background values.
-        for i,mcat_bg in enumerate([ 472.27884811, 472.68809896,
-                                     473.12396211, 473.22441377,
-                                     473.37736672, 473.21383515,
-                                     471.49669346, 470.74690756,
-                                     470.56233932, 468.12250625,
-                                     468.45463265, 468.20264455,
-                                     201.15752093]):
+        for i,mcat_bg in enumerate(
+                                   [ 467.93802676, 468.33843609, 468.76286039,
+                                     468.86925093, 469.01876064, 468.8591898,
+                                     467.19104717, 466.41687855, 466.27991533,
+                                     463.84206572, 464.17653866, 463.93932338,
+                                     196.65157325 ]):
             self.assertAlmostEqual(out['mcat_bg'][i],mcat_bg)
 
     def test_lcurve_query_FUV(self):
@@ -135,18 +139,20 @@ class TestRegression(unittest.TestCase):
         for binsize in (out['t1']-out['t0'])[:-1]:
             self.assertAlmostEqual(binsize,self.stepsz)
         # Check the magnitudes (no bg subtraction)
-        for i,mag in enumerate([ 18.8824072, 18.51488516, 18.58549817,
-                                 18.49929593, 18.08761381, 16.85940851,
-                                 13.17413591, 13.066734, 13.08552948,
-                                 12.0927487, 12.67582645, 13.21586764,
-                                 13.61488068 ]):
+        for i,mag in enumerate(
+                               [ 18.87976244, 18.50287132, 18.57996131,
+                                 18.49695605, 18.07555158, 16.86195021,
+                                 13.17351786, 13.04236207, 13.0682284,
+                                 12.09391959, 12.64711125, 13.23408717,
+                                 13.54495825 ]):
             self.assertAlmostEqual(out['mag'][i],mag)
         # Check the annulus background-subtracted fluxes (as magnitudes).
-        for i,mag_bgsub in enumerate([ 19.2232331, 18.73177926, 18.84716332,
-                                       18.72850653, 18.25984486, 16.92103755,
-                                       13.17962333, 13.0712827, 13.09040537,
-                                       12.09893898, 12.67990986, 13.22032368,
-                                       13.62084791 ]):
+        for i,mag_bgsub in enumerate(
+                                     [ 19.22936765, 18.7231948,  18.84792398,
+                                       18.73319054, 18.24969186, 16.92584474,
+                                       13.17918486, 13.04696094, 13.07317331,
+                                       12.10031926, 12.6511912, 13.23874903,
+                                       13.55070637 ]):
             self.assertAlmostEqual(out['mag_bgsub'][i],mag_bgsub)
         # Check the annulus background values (counts, flat-weighted, and
         # within aperture).
@@ -154,35 +160,39 @@ class TestRegression(unittest.TestCase):
                                        546., 501., 530., 1672., 644., 427.,
                                        170. ]):
             self.assertAlmostEqual(out['bg_counts'][i],bg_counts)
-        for i,bg_flat_counts in enumerate([ 125.69362214, 118.54809357,
-                                            131.40057948, 126.41933067,
-                                            142.39169606, 166.00599403,
-                                            451.45002048, 413.37614878,
-                                            435.43888582, 1376.0788091,
-                                            531.69121736, 352.91165538,
-                                            140.61735056 ]):
+        for i,bg_flat_counts in enumerate(
+                                          [ 127.47075602, 120.36695248,
+                                            133.53371262, 128.87274145,
+                                            144.00702825, 169.82327414,
+                                            461.82457166, 423.16061006,
+                                            444.22955368, 1406.82880576,
+                                            540.05583777, 359.42448472,
+                                            141.12829624 ]):
             self.assertAlmostEqual(out['bg_flat_counts'][i],bg_flat_counts)
-        for i,bg in enumerate([ 25.13872443, 23.70961871, 26.2801159,
-                                25.28386613, 28.47833921, 33.20119881,
-                                90.2900041, 82.67522976, 87.08777716,
-                                275.21576182, 106.33824347, 70.58233108,
-                                28.12347011 ]):
+        for i,bg in enumerate(
+                              [ 25.4941512,   24.0733905,   26.70674252,
+                                25.77454829,  28.80140565,  33.96465483,
+                                92.36491433,  84.63212201,  88.84591074,
+                                281.36576115, 108.01116755, 71.88489694,
+                                28.22565925 ]):
             self.assertAlmostEqual(out['bg'][i],bg)
         # Check the MCAT per-visit background subtracted fluxes.
-        for i,mag_mcatbgsub in enumerate([ 19.66522207, 19.01002047,
-                                           19.1236305, 18.98552466,
-                                           18.39572795, 16.94960235,
-                                           13.17704485, 13.06936863,
-                                           13.08821018, 12.09382223,
-                                           12.67766382, 13.21889073,
-                                           13.61924911 ]):
+        for i,mag_mcatbgsub in enumerate(
+                                         [ 19.65979013, 18.99112506,
+                                           19.11455631, 18.98186517,
+                                           18.37973659, 16.95236449,
+                                           13.17642514, 13.04493815,
+                                           13.07086667, 12.09499428,
+                                           12.64890063, 13.23716149,
+                                           13.5490537 ]):
             self.assertAlmostEqual(out['mag_mcatbgsub'][i],mag_mcatbgsub)
         # Check the MCAT per-visit background values.
-        for i,mcat_bg in enumerate([ 47.93542943, 47.94986719, 47.95818734,
-                                     47.95818213, 47.96663783, 47.96217278,
-                                     47.92064499, 47.92810498, 47.92805806,
-                                     47.84092321, 47.89754301, 47.91637805,
-                                     20.60352662]):
+        for i,mcat_bg in enumerate(
+                                   [ 47.45846584, 47.4727517,  47.48119299,
+                                     47.4810555,  47.48947085, 47.48532284,
+                                     47.44526692, 47.45122304, 47.45240596,
+                                     47.36531334, 47.42138539, 47.44004754,
+                                     20.12567625 ]):
             self.assertAlmostEqual(out['mcat_bg'][i],mcat_bg)
 
     def test_coadd_query_NUV(self):
@@ -198,7 +208,7 @@ class TestRegression(unittest.TestCase):
         # Exposure time of coadd must be sum of individual visits.
         self.assertEqual(sum(out2['exptime']), out['exptime'][0])
         # Check value of coadded magnitude.
-        self.assertAlmostEqual(out['mag'][0],14.24326228)
+        self.assertAlmostEqual(out['mag'][0],14.06593323)
         # Check that the uncertaintiy in the mag is less than the individual
         # visits.
         for x in out2['mag_err_1']:
@@ -219,7 +229,7 @@ class TestRegression(unittest.TestCase):
         # Exposure time of coadd must be sum of individual visits.
         self.assertEqual(sum(out2['exptime']), out['exptime'][0])
         # Check value of coadded magnitude.
-        self.assertAlmostEqual(out['mag'][0],14.20268608)
+        self.assertAlmostEqual(out['mag'][0],14.14882565)
         # Check that the uncertaintiy in the mag is less than the individual
         # visits.
         for x in out2['mag_err_1']:
