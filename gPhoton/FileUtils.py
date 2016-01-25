88f660245c360ab17408922e11aa6fae88d19b32
Scott Fleming <fleming@stsci.edu>
2016-01-25 13:07:39 -0500

    - PEP8 and docstring added.

--- gPhoton/FileUtils.py
+++ gPhoton/FileUtils.py
@@ -1,79 +1,160 @@
+"""
+.. module:: FileUtils
+
+   :synopsis: Methods for reading data from files or web queries.
+
+.. moduleauthor:: Chase Million <chase.million@gmail.com>
+"""
+
+# @CHASE - We need to avoid "import *", can we do specific imports here?@
 import csv
 from astropy.io import fits as pyfits
 import numpy as np
 from sys import stdout
-
-# A package for html queries
 import requests
 from MCUtils import *
 import gQuery
 
+# ------------------------------------------------------------------------------
 def load_raw6(raw6file):
-	"""Reads a raw6 file. Just wraps some pyfits commands."""
-	print "		",raw6file
-	hdulist = pyfits.open(raw6file,memmap=1)
-	htab = hdulist[1].header
-	hdulist.close()
-	return htab, hdulist
+    """
+    Reads a raw6 file. Just wraps some pyfits commands.
 
+    :param raw6file: Name of the raw6 file to read.
+
+    :type raw6file: str
+
+    :returns: tuple -- A two-element tuple containing the header of the
+    first extension, and the HDUList object returned from astropy.io.fits.
+    """
+
+    print "		", raw6file
+    hdulist = pyfits.open(raw6file, memmap=1)
+    htab = hdulist[1].header
+    hdulist.close()
+    return htab, hdulist
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
 def load_aspect(aspfile):
-	"""Loads an aspect file into a bunch of arrays."""
-	ra,dec,twist,time,aspflags=np.array([]),np.array([]),np.array([]),np.array([]),np.array([])
-	header = {'RA':[],'DEC':[],'ROLL':[]}
-	for i in xrange(len(aspfile)):
-        	print "         ",aspfile[i]
-		hdulist = pyfits.open(aspfile[i],memmap=1)
-		ra = np.append(ra,np.array(hdulist[1].data.field('ra')))
-		dec = np.append(dec,np.array(hdulist[1].data.field('dec')))
-		twist = np.append(twist,np.array(hdulist[1].data.field('roll')))
-		time = np.append(time,np.array(hdulist[1].data.field('t')))
-		aspflags = np.append(aspflags,np.array(hdulist[1].data.field('status_flag')))
-		header['RA'] = np.append(header['RA'],np.zeros(len(hdulist[1].data.field('ra')))+hdulist[0].header['RA_CENT'])
-		header['DEC'] = np.append(header['DEC'],np.zeros(len(hdulist[1].data.field('dec')))+hdulist[0].header['DEC_CENT'])
-		header['ROLL'] = np.append(header['ROLL'],np.zeros(len(hdulist[1].data.field('roll')))+hdulist[0].header['ROLL'])
-	#header = {'RA':np.array(ra).mean(),'DEC':np.array(dec).mean(),'ROLL':np.array(twist).mean()}
+    """
+    Loads a set of aspect files into a bunch of arrays.
 
-	# Time sort stuff.
-	ix = np.argsort(time)
+    :param aspfile: List of aspect files to read.
+    @CHASE - What happens if a scalar is given?@
 
-	hdulist.close()
-	#header['RA'] = header['RA'][ix]
-	#header['DEC'] = header['DEC'][ix]
-	#header['ROLL'] = header['ROLL'][ix]
+    :type aspfile: list @CHASE - verify this is a list or numpy.ndarray@
 
-	return ra[ix], dec[ix], twist[ix], time[ix], header, aspflags[ix]
+    :returns: tuple -- Returns a six-element tuple containing the RA, DEC,
+    twist, time, header, and aspect flags. Each of these EXCEPT for header,
+    are returned as numpy.ndarrays. The header is returned as a dict
+    containing the RA, DEC, and roll from the headers of the aspec files in
+    numpy.ndarrays.
+    @CHASE - What's a 'twist' in this context?@
+    """
 
-def web_query_aspect(eclipse,retries=20):
-	"""Grabs the aspect data from MAST databases based on eclipse."""
-	print "Attempting to query MAST database for aspect records."
-	entries = gQuery.getArray(gQuery.aspect_ecl(eclipse),retries=retries)
-	n = len(entries)
-	print '		Located '+str(n)+' aspect entries.'
-	if not n:
-		print "No aspect entries for eclipse "+str(eclipse)
-		return
-	ra,dec,twist,time,flags=[],[],[],[],[]
-	header = {'RA':[],'DEC':[],'ROLL':[]}
-	ra0,dec0,twist0 = [],[],[]
-	for i in xrange(n):
-		# The times are *1000 in the database to integerify
-		time.append(float(entries[i][2])/1000.)
-		ra.append(float(entries[i][3]))
-		dec.append(float(entries[i][4]))
-		twist.append(float(entries[i][5]))
-		flags.append(float(entries[i][6]))
-		ra0.append(float(entries[i][7]))
-		dec0.append(float(entries[i][8]))
-		twist0.append(float(entries[i][9]))
+    ra, dec = np.array([]), np.array([])
+    twist, time, aspflags = np.array([]), np.array([]), np.array([])
 
-	# Need to sort the output so that it is time ordered before returning.
-	# Although it should already be ordered by time because that is requested
-	#  in the SQL query above. If this is time consuming, remove it.
-	ix = np.argsort(np.array(time))
-	header = {'RA':np.array(ra0)[ix],'DEC':np.array(dec0)[ix],'ROLL':np.array(twist0)[ix]}
+    header = {'RA':[], 'DEC':[], 'ROLL':[]}
+    for i in xrange(len(aspfile)):
+        print "         ", aspfile[i]
+        hdulist = pyfits.open(aspfile[i], memmap=1)
+        ra = np.append(ra, np.array(hdulist[1].data.field('ra')))
+        dec = np.append(dec, np.array(hdulist[1].data.field('dec')))
+        twist = np.append(twist, np.array(hdulist[1].data.field('roll')))
+        time = np.append(time, np.array(hdulist[1].data.field('t')))
+        aspflags = np.append(aspflags,
+                             np.array(hdulist[1].data.field('status_flag')))
+        header['RA'] = np.append(header['RA'],
+                                 np.zeros(len(hdulist[1].data.field('ra')))+
+                                 hdulist[0].header['RA_CENT'])
+        header['DEC'] = np.append(header['DEC'],
+                                  np.zeros(len(hdulist[1].data.field('dec')))+
+                                  hdulist[0].header['DEC_CENT'])
+        header['ROLL'] = np.append(header['ROLL'],
+                                   np.zeros(len(hdulist[1].data.field('roll')))+
+                                   hdulist[0].header['ROLL'])
 
-	return np.array(ra)[ix],np.array(dec)[ix],np.array(twist)[ix],np.array(time)[ix],header,np.array(flags)[ix]
+    # Sort by time.
+    ix = np.argsort(time)
 
-def create_SSD_filename(band,eclipse):
-	"""Returns the Stim Separation Data (SSD) calibration file name."""
-	return "SSD_"+band.lower()+"_"+str(eclipse)+".tbl"
+    hdulist.close()
+
+    return ra[ix], dec[ix], twist[ix], time[ix], header, aspflags[ix]
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+def web_query_aspect(eclipse, retries=20):
+    """
+    Grabs the aspect data from MAST databases based on eclipse.
+
+    :param eclipse: The set of eclipses to retrieve aspect files for.
+    @CHASE - Is this a single eclipse or multiple eclipses?@
+
+    :type eclipse: int @CHASE - Confirm data type please.@
+
+    :param retries: The number of times to retry a query before giving up.
+
+    :type retries: int
+
+    :returns: tuple -- Returns a six-element tuple containing the RA, DEC,
+    twist, time, header, and aspect flags. Each of these EXCEPT for header,
+    are returned as numpy.ndarrays. The header is returned as a dict
+    containing the RA, DEC, and roll from the headers of the aspec files in
+    numpy.ndarrays.
+    @CHASE - What's a 'twist' in this context?@
+    """
+
+    print "Attempting to query MAST database for aspect records."
+    entries = gQuery.getArray(gQuery.aspect_ecl(eclipse), retries=retries)
+    n = len(entries)
+    print '		Located '+str(n)+' aspect entries.'
+    if not n:
+        print "No aspect entries for eclipse "+str(eclipse)
+        return
+    ra, dec, twist, time, flags = [], [], [], [], []
+    header = {'RA':[], 'DEC':[], 'ROLL':[]}
+    ra0, dec0, twist0 = [], [], []
+    for i in xrange(n):
+        # The times are *1000 in the database to integerify
+        time.append(float(entries[i][2])/1000.)
+        ra.append(float(entries[i][3]))
+        dec.append(float(entries[i][4]))
+        twist.append(float(entries[i][5]))
+        flags.append(float(entries[i][6]))
+        ra0.append(float(entries[i][7]))
+        dec0.append(float(entries[i][8]))
+        twist0.append(float(entries[i][9]))
+
+    # Need to sort the output so that it is time ordered before returning.
+    # Although it should already be ordered by time because that is requested
+    #  in the SQL query above. If this is time consuming, remove it.
+    ix = np.argsort(np.array(time))
+    header = ({'RA':np.array(ra0)[ix], 'DEC':np.array(dec0)[ix],
+               'ROLL':np.array(twist0)[ix]})
+
+    return (np.array(ra)[ix], np.array(dec)[ix], np.array(twist)[ix],
+            np.array(time)[ix], header, np.array(flags)[ix])
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+def create_SSD_filename(band, eclipse):
+    """
+    Returns the Stim Separation Data (SSD) calibration file name.
+    @CHASE - The use of camelcase is discouraged, should make this
+    'create_ssd_filename' but also need to change everywhere it is called.@
+
+    :param band: The band to create the SSD for, either 'FUV' or 'NUV'.
+
+    :type band: str
+
+    :param eclipse: The eclipse number to create the SSD file for.
+
+    :type eclipse: int @CHASE - int/float/long?@
+
+    :returns: str -- The name of the SSD file to create.
+    """
+
+    return "SSD_"+band.lower()+"_"+str(eclipse)+".tbl"
+# ------------------------------------------------------------------------------
