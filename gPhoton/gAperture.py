e56e228212441c6566378a4fea949fa141123548
Scott Fleming <fleming@stsci.edu>
2016-02-02 17:20:09 -0500

    - Added PEP8 and docstrings.

--- gPhoton/gAperture.py
+++ gPhoton/gAperture.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python
+
+"""
+.. module:: gAperture
+
+   :synopsis: Create light curves with user-defined time bins and photometric
+   apertures.
+
+.. moduleauthor:: Chase Million <chase.million@gmail.com>
+"""
+
 import os
 import ast
 import sys
@@ -11,14 +21,94 @@ from dbasetools import suggest_parameters
 from galextools import aper2deg
 from gPhoton import __version__
 
-def gAperture(band,skypos,radius,csvfile=None,annulus=None, coadd=False,
-              stepsz=False,verbose=0,overwrite=False,trange=None,tranges=None,
-              minexp=1.,maxgap=1500.,iocode='wb',photonfile=None,detsize=1.1,
-              minimal_output=False):
-    """Runs gAperture and returns the data in a python dict() and as
-    a CSV file if outfile is specified. Can be called from the interpreter.
+# ------------------------------------------------------------------------------
+def gAperture(band, skypos, radius, csvfile=None, annulus=None, coadd=False,
+              stepsz=False, verbose=0, overwrite=False, trange=None,
+              tranges=None, minexp=1., maxgap=1500., iocode='wb',
+              photonfile=None, detsize=1.1, minimal_output=False):
     """
-    if verbose>1:
+    Creates a light curve and returns the data in a python dict() and as
+    a CSV file, if outfile is specified. Can be called from the interpreter.
+
+    :param band: The band being used, either 'FUV' or 'NUV'.
+
+    :type band: str
+
+    :param skypos: The right ascension and declination, in degrees.
+
+    :type skypos: list @CHASE - list or numpy.ndarray?@
+
+    :param radius: The photometric aperture, in degrees. @CHASE - Confirm
+    this is in degrees.@
+
+    :type radius: float
+
+    :param csvfile: Name of the photon event CSV file to use.
+
+    :type csvfile: str
+
+    :param annulus: Radius of the inner and outer annuli to define the
+    background with, in degrees. @CHASE - confirm units.@
+
+    :type annulus: list
+
+    :param coadd: Set to True if calculating a total flux instead of flux
+    from each time bin.
+
+    :type coadd: bool
+
+    :param stepsz: The size of the time bins to use, in seconds.
+
+    :type stepsz: float @CHASE - The default should be None or 0., not a bool.@
+
+    :param verbose: Verbosity level, a value of 0 is minimum verbosity.
+
+    :type verbose: int
+
+    :param overwrite: If True, overwite an existing output file.
+
+    :type overwrite: bool
+
+    :param trange: Minimum and maximum time range to make a light curve.
+    @CHASE - assume this is in GALEX time?@
+
+    :type trange: list
+
+    :param tranges: Set of time ranges to query within. @CHASE - GALEX time?@
+
+    :type tranges: list
+
+    :param minexp: Minimum gap size, in seconds, for data to be considered
+    contiguous.
+
+    :type minexp: float
+
+    :param maxgap: Maximum gap size, in seconds, for data to be considered
+    contiguous.
+
+    :type maxgap: float
+
+    :param iocode: The code to use when writing the output file.
+
+    :type iocode: str
+
+    :param photonfile: Name of photon event CSV file to use.
+
+    :type photonfile: str
+
+    :param detsize: Effective diameter, in degrees, of the field-of-view.
+
+    :type detsize: float
+
+    :param minimal_output: If True, produce an output file with a minimum
+    number of columns.
+
+    :type minimal_output: bool
+
+    :returns: dict -- The light curve, including input parameters.
+    """
+
+    if verbose > 1:
         print "Using v{v} of gAperture.".format(v=__version__)
         print "Generating a light curve with the following paramters:"
         print " band:    {band}".format(band=band)
@@ -29,151 +119,268 @@ def gAperture(band,skypos,radius,csvfile=None,annulus=None, coadd=False,
         print " stepsz:  {stepsz}".format(stepsz=stepsz)
         print " csvfile: {csvfile}".format(csvfile=csvfile)
         print " verbose: {verbose}".format(verbose=verbose)
+
         if photonfile:
             print "Using local photon file: {photonfile}".format(
-                                                        photonfile=photonfile)
-    data = ct.write_curve(band.upper(), skypos[0], skypos[1], radius,
-        csvfile=csvfile, annulus=annulus, stepsz=stepsz, verbose=verbose,
-        overwrite=overwrite, trange=trange, tranges=tranges,coadd=coadd,
-        minexp=minexp, maxgap=maxgap, iocode = iocode, detsize=detsize,
-        minimal_output=minimal_output)
-    return data
+                photonfile=photonfile)
 
+    data = ct.write_curve(band.upper(), skypos[0], skypos[1], radius,
+                          csvfile=csvfile, annulus=annulus, stepsz=stepsz,
+                          verbose=verbose, overwrite=overwrite, trange=trange,
+                          tranges=tranges, coadd=coadd, minexp=minexp,
+                          maxgap=maxgap, iocode=iocode, detsize=detsize,
+                          minimal_output=minimal_output)
+
+    return data
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
 def check_radius(args):
-    """Checks the radius value."""
+    """
+    Checks the radius value.
+
+    :param args: The command-line arguments.
+
+    :type args: argparse.ArgumentParser Namespace
+
+    :returns: argparse.ArgumentParser Namespace -- The updated command-line
+    arguments.
+    """
+
     if not (args.radius or args.suggest or args.aperradius):
         print "Must specify an aperture radius."
         raise SystemExit
+
     if args.radius and args.aperradius:
         print "Must not specify both --aperture and --mcataper."
         raise SystemExit
-    if args.aperradius and not args.radius:
-        args.radius=aper2deg(args.aperradius)
-    return args
 
+    if args.aperradius and not args.radius:
+        args.radius = aper2deg(args.aperradius)
+
+    return args
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
 def check_annulus(args):
-    """Checks and formats the annulus values."""
+    """
+    Checks and formats the annulus values.
+
+    :param args: The command-line arguments.
+
+    :type args: argparse.ArgumentParser Namespace
+
+    :returns: argparse.ArgumentParser Namespace -- The updated command-line
+    arguments.
+    """
+
     if not (args.annulus1 and args.annulus2) and not args.annulus:
         args.annulus = None
     elif args.annulus1 and args.annulus2:
-        args.annulus = [args.annulus1,args.annulus2]
+        args.annulus = [args.annulus1, args.annulus2]
     else:
         args.annulus = list(args.annulus)
         if not (args.annulus1 and args.annulus2):
             args.annulus1 = args.annulus[0]
             args.annulus2 = args.annulus[1]
+
     return args
+# ------------------------------------------------------------------------------
 
+# ------------------------------------------------------------------------------
 def setup_parser(iam='gaperture'):
-    """Defines command line arguments."""
-    parser = argparse.ArgumentParser(description="Generate a light curve")
-    parser = gargs.common_args(parser,iam)
-    parser.add_argument("-a", "--aperture", action="store", dest="radius",
-        help="Aperture radius in decimal degrees",
-        type=float)
-    parser.add_argument("--mcataper", "--apercode", action="store",
-        dest="aperradius", type=int)
-    parser.add_argument("-i", "--inner", action="store", dest="annulus1",
-        help="Inner annulus radius for background subtraction", type=float)
-    parser.add_argument("-o", "--outer", action="store", dest="annulus2",
-        help="Outer annulus radius for background subtraction", type=float)
-    parser.add_argument("--annulus", action="store", type=ast.literal_eval,
-        dest="annulus",
-        help="Annulus inner and outer radius definition as '[inner,outer]'")
-    parser.add_argument("-f", "--file", "--outfile", "--csvfile",
-        action="store", type=str, dest="csvfile", help="CSV output file")
-    parser.add_argument("--stamp", action="store", type=str, dest="stamp",
-        help="Filename for a JPEG preview stamp of the targeted region.")
-    parser.add_argument("--addhdr", action="store_true", dest="addhdr",
-        help="Add command line and column names to the top of the .csv file.")
-    parser.add_argument("--iocode", action="store", dest="iocode", default="wb",
-        help="The iocode to be passed to the cvs writer. Don't mess with this.",
-        type=str)
-    parser.add_argument("--bgmaskdepth", action="store", dest="maskdepth",
-        help="DEPRECATED. Depth of the background mask in AB Magnitudes.",
-        type=float, default=20.0)
-    parser.add_argument("--bgmaskradius", action="store", dest="maskradius",
-        help="DEPRECATED. Radius of background mask in n sigmas (assuming Gaussians)",
-        type=float, default=1.5)
-    parser.add_argument("--minimal_output", "--minout", action="store_true",
-        dest="minimal_output", help=("If csvfile is also set, writes only "+
-        "a small number of human-readable columns to the lightcurve file."))
-    return parser
-
-def check_args(args,iam='gaperture'):
-    """Checks validity of command line arguments and, in some cases
-    massages them a little bit.
     """
-    args = gargs.check_common_args(args,iam)
+    Defines command line arguments.
+
+    :param iam: The name of the method being called.
+
+    :type iam: str
+
+    :returns: argparse.ArgumentParser Namespace -- The command-line arguments.
+    """
+
+    parser = argparse.ArgumentParser(description="Generate a light curve")
+    parser = gargs.common_args(parser, iam)
+    parser.add_argument("-a", "--aperture", action="store", dest="radius",
+                        help="Aperture radius in decimal degrees", type=float)
+    parser.add_argument("--mcataper", "--apercode", action="store",
+                        dest="aperradius", type=int)
+    parser.add_argument("-i", "--inner", action="store", dest="annulus1",
+                        help="Inner annulus radius for background subtraction",
+                        type=float)
+    parser.add_argument("-o", "--outer", action="store", dest="annulus2",
+                        help="Outer annulus radius for background subtraction",
+                        type=float)
+    parser.add_argument("--annulus", action="store", type=ast.literal_eval,
+                        dest="annulus", help="Annulus inner and outer radius"
+                        " definition as '[inner,outer]'")
+    parser.add_argument("-f", "--file", "--outfile", "--csvfile",
+                        action="store", type=str, dest="csvfile",
+                        help="CSV output file")
+    parser.add_argument("--stamp", action="store", type=str, dest="stamp",
+                        help="Filename for a JPEG preview stamp of the"
+                        " targeted region.")
+    parser.add_argument("--addhdr", action="store_true", dest="addhdr",
+                        help="Add command line and column names to the top of"
+                        " the .csv file.")
+    parser.add_argument("--iocode", action="store", dest="iocode", default="wb",
+                        help="The iocode to be passed to the cvs writer."
+                        " Don't mess with this.", type=str)
+    parser.add_argument("--bgmaskdepth", action="store", dest="maskdepth",
+                        help="DEPRECATED. Depth of the background mask in AB"
+                        " Magnitudes.", type=float, default=20.0)
+    parser.add_argument("--bgmaskradius", action="store", dest="maskradius",
+                        help="DEPRECATED. Radius of background mask in n"
+                        " sigmas (assuming Gaussians)", type=float, default=1.5)
+    parser.add_argument("--minimal_output", "--minout", action="store_true",
+                        dest="minimal_output", help=("If csvfile is also set,"
+                                                     " writes only a small"
+                                                     " number of human-"
+                                                     "readable columns to the"
+                                                     " lightcurve file."))
+
+    return parser
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+def check_args(args, iam='gaperture'):
+    """
+    Checks validity of command line arguments and, in some cases
+    massages them a little bit.
+
+    :param args: The command-line arguments.
+
+    :type args: argparse.ArgumentParser Namespace
+
+    :param iam: The name of the method being called.
+
+    :type iam: str
+
+    :returns: argparse.ArgumentParser Namespace -- The updated command-line
+    arguments.
+    """
+
+    args = gargs.check_common_args(args, iam)
     args = check_radius(args)
     args = check_annulus(args)
-    return args
 
+    return args
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
 def reconstruct_command(args):
-    """Rebuild the equivalent command line for this call to gAperture."""
+    """
+    Rebuild the equivalent command line for this call to gAperture.
+
+    :param args: The command-line arguments.
+
+    :type args: argparse.ArgumentParser Namespace
+
+    :returns: str -- The equivalent command-line for this call to gAperture.
+    """
+
     cmd = " ".join(sys.argv)
+
     if args.suggest:
         cmd = "{cmd} --annulus [{i},{o}] --aperture {a}".format(
-                        cmd=cmd,i=args.annulus1,o=args.annulus2,a=args.radius)
+            cmd=cmd, i=args.annulus1, o=args.annulus2, a=args.radius)
+
     if args.verbose > 1:
         print cmd
-    return cmd
 
+    return cmd
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
 def setup_file(args):
-    """ If requested, create a header for the CSV that includes the column
-    names and a reconstruction of the command line call.
     """
+    If requested, create a header for the CSV that includes the column
+    names and a reconstruction of the command line call.
+
+    :param args: The command-line arguments.
+
+    :type args: argparse.ArgumentParser Namespace
+
+    :returns: argparse.ArgumentParser Namespace -- The updated command-line
+    arguments.
+    """
+
     if not args.csvfile:
         args.csvfile = False
     else:
-    # This initiates the file.
+        # This initiates the file.
         if not args.overwrite and os.path.exists(args.csvfile):
             print "{csvfile} exists. Pass --overwrite to replace it.".format(
-                                                          csvfile=args.csvfile)
+                csvfile=args.csvfile)
             raise SystemExit
-        f = open(args.csvfile,args.iocode)
+        f = open(args.csvfile, args.iocode)
         f.write('| v{v}\n'.format(v=__version__))
+
         if args.addhdr:
             if args.verbose:
                 print 'Recording command line construction to {f}'.format(
-                                                                f=args.csvfile)
+                    f=args.csvfile)
             # Write the command line to the top of the output file
             f.write('| '+reconstruct_command(args)+'\n')
         f.close()
         # Setting iocode to append to the file we just created
         args.iocode = 'ab'
-    return args
 
+    return args
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
 def stamp(args):
+    """
+    Creates a jpeg preview image of the aperture area. @CHASE - please update@
+
+    :param args: The command-line arguments.
+
+    :type args: argparse.ArgumentParser Namespace
+    """
+
     if not args.stamp:
         return
     else:
         if args.verbose:
             print 'Writing stamp preview to {f}'.format(f=args.stamp)
+
         width = 2.*(args.annulus2 if args.annulus2 else args.radius)
-        args.skyrange = [width,width]
+
+        args.skyrange = [width, width]
+
         write_jpeg(args.stamp, args.band, args.skypos, args.trange,
                    args.skyrange, width=False, height=False,
                    stepsz=args.stepsz, overwrite=args.overwrite,
                    verbose=args.verbose)
-        return
 
+        return
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
 def __main__():
-    """Called when gAperture is executed through the command line."""
+    """
+    Called when gAperture is executed through the command line.
+    """
+
     args = setup_parser().parse_args()
     args = check_args(args)
     args = setup_file(args)
     stamp(args)
-    # TODO: add support for trange(s)
+
+    # @CHASE - Is this (below) still an item that needs attention?
+    # [Future]: add support for trange(s).
     data = gAperture(args.band, args.skypos, args.radius, csvfile=args.csvfile,
                      annulus=args.annulus, stepsz=args.stepsz,
                      verbose=args.verbose, overwrite=args.overwrite,
-                     trange=[args.tmin,args.tmax], tranges=args.trange,
+                     trange=[args.tmin, args.tmax], tranges=args.trange,
                      coadd=args.coadd, minexp=args.minexp, maxgap=args.maxgap,
                      iocode=args.iocode, detsize=args.detsize,
                      minimal_output=args.minimal_output)
+# ------------------------------------------------------------------------------
 
 if __name__ == "__main__":
+    # @CHASE - What is 'pycurl' below? Is it defined? Missing an import?
     try:
         __main__()
     except (KeyboardInterrupt, pycurl.error):
