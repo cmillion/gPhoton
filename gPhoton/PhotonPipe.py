a3045c538d400f6e23bb822221e62c830fbf215e
Chase Million <chase.million@gmail.com>
2016-10-14 13:59:27 -0400

    compatibility

--- gPhoton/PhotonPipe.py
+++ gPhoton/PhotonPipe.py
@@ -540,6 +540,10 @@ def photonpipe(raw6file, scstfile, band, outbase, aspfile=None, ssdfile=None,
             if (thisflag == 2 or thisflag == 5 or thisflag == 7 or
                     thisflag == 8 or thisflag == 9 or thisflag == 10 or
                     thisflag == 11 or thisflag == 12):
+            # Should be:
+            #if ((thisflag == 3) or (thisflag == 6) or (thisflag == 8) or
+            #        (thisflag == 9) or (thisflag == 10) or
+            #        (thisflag == 11) or (thisflag = 12):)
                 if nullfile:
                     NULLspreadsheet.writerow(
                         [int(t[i]*dbscale), x[i], y[i],
