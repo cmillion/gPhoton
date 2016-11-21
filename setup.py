2d18926e1f92d8de04083127a1fad1813bac6847
Chase Million <chase.million@gmail.com>
2016-11-20 22:05:34 -0500

    updating pythong version and development status

--- setup.py
+++ setup.py
@@ -26,9 +26,9 @@ setup(
     zip_safe=False,
     license='AURA',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python',
         ],
     keywords=['astronomy', 'galex', 'photometry', 'ultraviolet'],
     scripts=['bin/gPipeline', 'bin/gAperture', 'bin/gFind', 'bin/gMap'],
